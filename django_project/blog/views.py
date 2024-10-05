3d9c6312b1b3b1806a68de39d82d4eb14858b0e7
Areel <areel15511@gmail.com>
2024-10-05 17:13:48 -0400

    getting posts from database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-    {
-        "author": "alice",
-        "date_posted": "October 5th, 2024",
-        "title": "Lorem Ipsum",
-        "content": "content"
-    },
-    {
-        "author": "bob",
-        "date_posted": "September 5th, 2024",
-        "title": "Lorem Ipsum",
-        "content": "content"
-    }
-]
-# Create your views here.
 def home(request):
-    context = {"posts": posts, "title": "Home"}
+    context = {
+        'posts': Post.objects.all()
+    }
     return render(request, "blog/home.html", context)
 
 def about(request):
